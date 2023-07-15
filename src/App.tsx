df275fa864401c0a90db3d7d21ae29d0a387a7f0
Arpit Mishra <106508210+arpitmiahra4@users.noreply.github.com>
2023-07-15 20:57:56 +0530

    :structure updaed

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Navbar from "./Components/navbarFooter/Navbar";
+import AllRoutes from "./Routes/AllRoutes";
+import Footer from "./Components/navbarFooter/Footer";
 
 function App() {
   return (
     <div className="App">
-  
+      <header>
+        <nav>
+          <Navbar />
+        </nav>
+      </header>
+      <center>
+        <AllRoutes />
+      </center>
+      <footer>
+        <Footer />
+      </footer>
     </div>
   );
 }
